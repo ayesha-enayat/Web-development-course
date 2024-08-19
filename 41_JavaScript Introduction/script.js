ee5c7a6c673e2fa1ca130e6e5bfe1dfa0331c081
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-08-19 14:11:01 +0500

    JavaScript Introduction

---
+++ 41_JavaScript Introduction/script.js
@@ -0,0 +1,10 @@
+alert("hello world");
+console.log("hello world");
+var a=prompt("enter your name");
+var b=confirm("are you sure you wnat to quit");
+if(b){
+    console.log("good bye");
+}
+else{
+    console.log("stay connected");
+}
