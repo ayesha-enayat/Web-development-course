32a58f9e2b5974569e48d685e2a4b98fd332a2f2
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-08-20 19:06:27 +0500

    practice set 1

---
+++ 43_Practice set 1/index.js
@@ -0,0 +1,42 @@
+//***********Quick Quiz*********************
+
+
+// Write a javascript program to store name , phone number and marks of a student using object
+
+let  student = {
+    name:"Ayesha",
+    phone_number:1234567890,
+    marks:90,
+}
+console.log(student);
+
+//************chapter 1 practice set******************
+
+//create a variable of type string and try to add a number to it
+
+let myname="ayesha";
+myname=myname+12;
+console.log(myname);
+
+//use type of operator to find the data type of string  in last question
+console.log(typeof myname);
+
+//create a constant object in javascript can you change it to hold a new number later?
+
+/*const myconstant={
+    constant=20,
+};
+constant=40;*/
+
+//write a js program to create a word meaning dictionary of 5 words
+let dictionary={
+    sky:"asman",
+    beautiful:"khoobsurat",
+    rest:"araam",
+    flower:"phool",
+    fruit:"phal",
+
+}
+console.log(dictionary);
+
+
