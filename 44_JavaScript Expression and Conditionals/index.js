a249f084ecd8434d357bcceccc4fa3b809775e88
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-08-20 20:48:13 +0500

    update code

--- 44_JavaScript Expression and Conditionals/index.js
+++ 44_JavaScript Expression and Conditionals/index.js
@@ -94,3 +94,47 @@ age=16;
 if(age<=18){ //greater than or equal to
     console.log("you are less than 18");
 }
+
+
+// 3----------- Logical operators---------------
+if(age>18 && age==18){  //logical AND
+    console.log("you are above 18");
+}
+
+if(age<18 || age==18){  //logical OR
+    console.log("you are 18 or turning to 18");
+}
+
+if(age!=17){      //logical not
+    console.log("you are not 17");
+}
+
+
+
+/****************************conditional statements*************************** */
+//if else if  else ladder
+let myage=24;
+if(myage>10){
+    console.log("you are a valid age");
+}
+else if(myage>10 && myage<15){
+    console.log("you are a teenager");
+}
+else if(myage>18){
+    console.log("you are 18 years old");
+}
+else{
+    console.log("invalid age");
+}
+
+
+
+/****************************JavaScript ternary operator*************************** */
+const Myage = 26;
+const beverage = Myage >= 21 ? "Beer" : "Juice";
+console.log(beverage); // "Beer"
+
+
+const marks=87;
+const  grade = marks >= 80 ? "A" : "pass";
+console.log(grade); // "A"
