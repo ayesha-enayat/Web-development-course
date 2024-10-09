fb0add371ab17156241c22cdacbf42856a554dcb
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-10-09 10:08:21 +0500

    js arrays practice

--- 52_JavaScript Arrays/index.js
+++ 52_JavaScript Arrays/index.js
@@ -64,7 +64,7 @@
 
 //Loops in Array
 
-let array=[1,2,3,4,5,6];
+// let array=[1,2,3,4,5,6];
 // for (let index = 0; index < array.length; index++) {
 //     const element = array[index];
 //     console.log(element);
@@ -95,6 +95,28 @@ let array=[1,2,3,4,5,6];
 // }
 
 
-for (const element of array) {
-    console.log(element)
-}
+// for (const element of array) {
+//     console.log(element)
+// }
+
+// let arr=[1,3,5,7,11,13];
+// let newArr=arr.map((e)=>{
+//     return e**2;
+// })
+// console.log(newArr);
+
+// const greaterThanSeven=(e)=>{
+//     if(e>7){
+//         return true
+//     }
+//     return false
+// }
+// console.log(newArr.filter(greaterThanSeven))
+
+
+// let arr2=[1,2,3,4,5,6]
+// const red= (a,b)=>{
+// return a+b;
+// }
+// console.log(arr2.reduce(red));
+
