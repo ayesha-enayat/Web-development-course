e6c2b4ed25a40551a01400196d8b61de73502f49
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-10-14 03:41:20 +0500

    calculate factorial

--- 52_JavaScript Arrays/index.js
+++ 52_JavaScript Arrays/index.js
@@ -114,9 +114,9 @@
 // console.log(newArr.filter(greaterThanSeven))
 
 
-// let arr2=[1,2,3,4,5,6]
-// const red= (a,b)=>{
-// return a+b;
-// }
-// console.log(arr2.reduce(red));
+let arr2=[1,2,3,4,5,6]
+const red= (a,b)=>{
+return a+b;
+}
+console.log(arr2.reduce(red));
 
---
+++ 54_JavaScript Ex 11/index.js
@@ -0,0 +1,9 @@
+/*Calculate Factorial */
+var numb=4;
+var Factorial=1;  //4x3x2x1  //1x2x3x4
+for(i=1;i<=numb;i++){
+    Factorial=Factorial*i;       
+}
+console.log(`Factorial of ${numb} is ${Factorial}`);
+
+
